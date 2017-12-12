338964ae1164a8b41d6550b3cf1ba7f3922310db
github username <github email address>
2017-12-12 12:22:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers)
+{
+  let newArray = []
+  for(driver in drivers)
+  {
+     newArray.push(lcCallback(drivers[driver]));
+  }
+  return newArray;
+}
+function lcCallback(item)
+{
+  return item.toLowerCase();
+}
+
+
+function nameToAttributes (list) {
+  return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
+  });
+}
+
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (driver) {
+    return `${driver.name} is from ${driver.hometown}`;
+  });
+}
