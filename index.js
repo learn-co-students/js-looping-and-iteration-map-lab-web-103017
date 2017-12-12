6bf9df92c06fc30d84da5399b6a2fc3ff1b358d3
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-11 19:43:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(name){
+    return name.toLowerCase();
+  });
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(firstlast){
+    const firstname = firstlast.split(" ")[0]
+    const lastname = firstlast.split(" ")[1]
+      return {firstName: firstname, lastName: lastname}
+  });
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver){
+    return `${driver.name} is from ${driver.hometown}`
+  });
+}
