3fca0f3bfe4713a5b4d919c950e6e04b80efede8
github username <github email address>
2017-12-11 21:59:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function(name){
+    return name.toLowerCase()
+  })
+}
+
+function nameToAttributes(arr){
+  return arr.map(function(name){
+    return {firstName: name.split(" ")[0], lastName: name.split(" ")[1]}
+  })
+}
+
+function attributesToPhrase(arr){
+  return arr.map(function(hash){
+    return `${hash.name} is from ${hash.hometown}`
+  })
+}
